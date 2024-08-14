1939912ee4aafa01dfac577248b0b461fcc28c9c
nahidraz23 <nzamanraz@gmail.com>
2024-08-15 00:33:27 +0600

    footer implemented

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navabar from '../componenets/Navabar';
+import Footer from '../componenets/Footer';
 
 const MainLayout = () => {
     return (
         <div>
             <Navabar></Navabar>
-            <Outlet></Outlet>
+            <div className='h-screen'>
+                <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
